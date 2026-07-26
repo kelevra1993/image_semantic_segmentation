2884626a9533f03aade469bdab40efaf186fc1f0
Robert Kelevra DGX Spark <kelevra.robert@gmail.com>
2026-07-27 00:25:28 +0200

    [UltraSound] Update main and trainer.py

--- ultrasound_segmentation/main.py
+++ ultrasound_segmentation/main.py
@@ -11,17 +11,9 @@ def main():
     # Load configuration
     experiment_configuration, model_configuration = load_experiment_configuration(experiment_configuration_path)
 
-
     # Initialize Trainer
-    trainer = Trainer(
-        project_root=experiment_configuration["project_root"],
-        model_configuration=model_configuration,
-        data_folder=experiment_configuration["data_folder"],
-        number_iterations=experiment_configuration["number_iterations"],
-        weight_saving_iterations=experiment_configuration["weight_saving_iterations"],
-        learning_rate=experiment_configuration["learning_rate"],
-        dtype=experiment_configuration["dtype"],
-        information_dump=experiment_configuration["information_dump"],)
+    trainer = Trainer(experiment_configuration=experiment_configuration,
+                      model_configuration=model_configuration)
 
     # Start training loop
     print(f"Starting Training For Experiment: {experiment_configuration['experiment_name']}...")
